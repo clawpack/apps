7c8900be861ca892b53ed2b92e0cdea8cfabb041
Kyle Mandli <kyle.mandli@gmail.com>
2012-05-02 15:25:32 -0500

    Shelf contour plotting is working

--- multi_layer/1d/shelf_contour.py
+++ multi_layer/1d/shelf_contour.py
@@ -4,128 +4,98 @@ import sys
 import os
 import re
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
 
-from pyclaw.plotters.data import ClawPlotData
-from pyclaw.data import Data
+from pyclaw.solution import Solution
+import visclaw.data as data
 
-if len(sys.argv) > 1:
-    out_dir = sys.argv[1]
-else:
-    out_dir = "_output"
+rho = [1025.0,1045.0]
+eta_init = [0.0,-300.0]
 
-# Plot settings
-claw_data = Data(os.path.join(out_dir,'claw.data'))
-prob_data = Data(os.path.join(out_dir,'problem.data'))
-pd = ClawPlotData()
-pd.outdir = out_dir
-
-# Load the bathymetry
-b =  np.loadtxt(os.path.join(pd.outdir,'fort.aux'),
-                converters={0:(lambda x:float(re.compile("[Dd]").sub("e",x)))})
-
-def read_data():
-    for num_frames in xrange(1000):
-        fname = os.path.join(out_dir,'fort.q%s' % str(num_frames).zfill(4))
-        if not os.path.exists(fname):
+def plot_contour(out_dir="./_output",num_layers=2,num_frames=1000,ref_lines=[-130e3,-30e3]):
+    """Plot a contour plot of a shelf based simluation"""
+    
+    # Create plot data
+    plot_data = data.ClawPlotData()
+    plot_data.outdir = out_dir
+    
+    # Read in bathymetry
+    sol = [Solution(0,path=out_dir,read_aux=True)]
+    b = sol[0].state.aux[0,:]
+    
+    # Extract x coordinates, this assumes that these do not change through the
+    # simluation (they should not)
+    x = sol[0].state.grid.dimensions[0].centers
+    
+    # Read in all solutions
+    print "Reading in solutions..."
+    for frame in xrange(1,num_frames):
+        try:
+            sol.append(Solution(frame,path=out_dir))
+        except IOError:
+            # We have reached the last frame before given num_frames reached
+            num_frames = frame - 1
             break
-    # num_frames = 301
-    mx = claw_data.mx
-    eta = np.ndarray((mx,num_frames,2))
+    print "Found %s frames to plot." % num_frames
+    
+    # Create plotting arrays
+    print "Constructing plotting variables..."
+    eta = np.ndarray((num_frames,num_layers,len(x)))
     t = np.ndarray((num_frames))
+    for frame in xrange(num_frames):
+        # Append data to eta and t lists
+        t[frame] = sol[frame].t / 3600.0
+        
+        # Calculate from the bottom up
+        layer_index = 2 * (num_layers-1)
+        eta[frame,num_layers - 1,:] = sol[frame].q[layer_index,:] / rho[-1] + b
+        
+        # Calculate the rest of the layers
+        for layer in xrange(num_layers-2,-1,-1):
+            layer_index = 2 * layer
+            eta[frame,layer,:] = sol[frame].q[layer_index,:] / rho[layer] + eta[frame,layer+1,:]
     
-    for frameno in xrange(num_frames):
-        frame = pd.getframe(frameno)
-        q = frame.grids[0].q
-        t[frameno] = frame.t / 3600.0
-        eta[:,frameno,1] = q[:,2] + b
-        eta[:,frameno,0] = q[:,0] + eta[:,frameno,1]
-    
-    x = frame.p_center[0]
+    # Create mesh grid for plot
     X,T = np.meshgrid(x,t)
-    return X,T,eta
-
-bathy_ref_lines = [-130e3,-30e3]
-
-def contour_plot(X,T,eta):
-    pylab.figure(1,figsize=[10,8])
-    pylab.clf()
-    pylab.subplot(1,2,1)
-    # pylab.axes([.1,.1,.6,.8])
-    clines = np.linspace(.025,.4,15)
-    # clines = np.linspace(.4,.4,15)
-    pylab.contour(X,T,eta[:,:,0].T,clines,colors='r')
-    # pylab.contour(X,T,eta,-clines,colors='b',linestyles='solid')
-    pylab.contour(X,T,-eta[:,:,0].T,clines,colors='b--')
-    for ref_line in bathy_ref_lines:
-        pylab.plot([ref_line,ref_line],[0,2],'k--')
-    pylab.xticks([-300e3,-200e3,-100e3,-30e3],[300,200,100,30],fontsize=15)
-    pylab.yticks(fontsize=15)
-    pylab.xlabel("Kilometers offshore",fontsize=15)
-    pylab.ylabel("Hours",fontsize=20)
-    pylab.title("Contours of top surface",fontsize=15)
-    pylab.xlim([-200e3,0.0])
-    # add_timeslices()
-    # fname = "_".join((out_dir,"shelf_top.png"))
-    # print "Writing out to %s" % fname
-    # pylab.savefig(fname)
     
-    # pylab.figure(2,figsize=[7,8])
-    # pylab.clf()
-    pylab.subplot(1,2,2)
-    # pylab.axes([.1,.1,.6,.8])
-    clines = np.linspace(.025,0.5,15)
-    pylab.contour(X,T,eta[:,:,1].T - prob_data.eta_2,clines,colors='r')
-    # # pylab.contour(X,T,eta[:,:,1].T,colors='r')
-    # pylab.contour(X,T,eta,-clines,colors='b',linestyles='solid')
-    pylab.contour(X,T,-(eta[:,:,1].T - prob_data.eta_2),clines,colors='b--')
-    for ref_line in bathy_ref_lines:
-        pylab.plot([ref_line,ref_line],[0,2],'k--')
-    pylab.xticks([-300e3,-200e3,-100e3,-30e3],[300,200,100,30],fontsize=15)
-    pylab.xlim([-200e3,0.0])
-    # # pylab.yticks(fontsize=15)
-    pylab.xlabel("Kilometers offshore",fontsize=15)
-    locs,labels = pylab.yticks()
-    # labels = np.flipud(locs)/1.e3
-    labels = ['' for i in xrange(len(locs))]
-    pylab.yticks(locs,labels)
-    # # pylab.ylabel("Hours",fontsize=20)
-    pylab.title("Contours of internal surface",fontsize=15)
-    # add_timeslices()
-    fname = "_".join((out_dir,"shelf_contour.png"))
-    print "Writing out to %s" % fname
-    pylab.savefig(fname)
+    # Plot the contours of each layer
+    clines = np.linspace(.025,.4,15)
+    title = ['top','internal']
+    print "Creating plots..."
+    fig = plt.figure(1,figsize=[10,8])
+    for layer in xrange(num_layers):
+        axes = fig.add_subplot(1,num_layers,layer+1)
 
-
-def add_timeslices():
-    times = pylab.array([0,200,400,600,1000,1400,2000,2800,3400,4800]) 
-    for t in times:
-        thours = t / 3600.
-        pylab.plot([-400e3,0.],[thours,thours],'k')
-        pylab.annotate('%s seconds' % t,[0.,thours],[30e3,thours],\
-              arrowprops={'width':1,'color':'k','frac':0.2,'shrink':0.1})
-
-
-def xt_and_frameplots(X,T,eta):
-    """
-    Unfinished attempt to plot frames with xt plot
-    """
-    contour_plot(X,T,eta)
-    framenos = [0,1,2,3,5,7,10,14,27,24]
-    plotdata = ClawPlotData()
-    plotdata = setplot(plotdata)
-    for frameno in framenos:
-        frametools.plotframe(frameno,plotdata)
-
-def mesh_plot(X,T,eta):
-    from enthought.mayavi import mlab
-    X = 2000*X/np.abs(x.min())
-    eta = 1000*eta
-    mlab.figure(1)
-    mlab.clf()
-    mlab.mesh(X,T,eta)
+        # Plot positive and negative contours
+        eta_plot = eta[:,layer,:] - eta_init[layer]
+        plot = axes.contour(X,T, eta_plot,clines,colors='r')
+        plot = axes.contour(X,T,-eta_plot,clines,colors='b',linestyle='dashed')
+        for ref_line in ref_lines:
+            axes.plot([ref_line,ref_line],[0,2],'k--')
+        
+        # X ticks and labels
+        axes.set_xticks([-300e3,-200e3,-100e3,-30e3])
+        axes.set_xticklabels([300,200,100,30],fontsize=15)
+        axes.set_xlabel("Kilometers offshore",fontsize=15)
+        axes.set_xlim([-200e3,0.0])
+        
+        # First plot from left to right, write y ticks
+        if layer == 0:
+            plt.yticks(fontsize=15)
+            axes.set_ylabel("Hours",fontsize=20)
+        else:
+            # Remove tick labels
+            axes.set_yticklabels(['' for label in axes.get_yticklabels()])
+        
+        axes.set_title("Contours of %s surface" % title[layer],fontsize=15)
+        
+    
+    file_name = "_".join((out_dir,"shelf_contour.png"))
+    print "Writing out to %s" % file_name
+    plt.savefig(file_name)
 
 if __name__=="__main__":
-    X,T,eta = read_data()
-    contour_plot(X,T,eta)
-    # pylab.savefig("shelfxt.eps")
+    if len(sys.argv) > 1:
+        plot_contour(*sys.argv[1:])
+    else:
+        plot_contour()
