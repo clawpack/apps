d1b0e7be6d3680068c39689c8754fd2f04922026
Kyle Mandli <kyle.mandli@gmail.com>
2012-06-01 15:28:12 -0500

    Sloped shelf working

--- multi_layer/1d/shelf.py
+++ multi_layer/1d/shelf.py
@@ -146,42 +146,119 @@ def sloped_shelf(num_cells,eigen_method,**kargs):
     name = 'sloped_shelf'
     outdir,plotdir,log_path = runclaw.create_output_paths(name,prefix,**kargs)
     
-    # Initialize common pieces of the solver and pass through all non-specific
-    # parameters
-    solver,solution,controller = ml.setup(lower=-400e3,upper=0.0,num_cells=num_cells,log_path=log_path)
+    # Redirect loggers
+    # This is not working for all cases, see comments in runclaw.py
+    for logger_name in ['io','solution','plot','evolve','f2py','data']:
+        runclaw.replace_stream_handlers(logger_name,log_path,log_file_append=False)
 
-    # Set wall boundary condition at beach
-    solver.bc_upper[0] = 0
-    solver.user_bc_upper = ml.wall_qbc_upper        
+    # Load in appropriate PyClaw version
+    if kargs.get('use_petsc',False):
+        import clawpack.petclaw as pyclaw
+    else:
+        import clawpack.pyclaw as pyclaw
+        
     
-    # Change pertinent problem data
-    solution.state.problem_data['eigen_method'] = eigen_method
-    solution.state.problem_data['inundation_method'] = 2
-    solution.state.problem_data['rho'] = [1025.0,1045.0]
-    solution.state.problem_data['rho_air'] = 1.15
-    solution.state.problem_data['r'] = solution.state.problem_data['rho'][0] \
-                                     / solution.state.problem_data['rho'][1]
-    solution.state.problem_data['one_minus_r'] = \
-                                        1.0 - solution.state.problem_data['r']
+    # =================
+    # = Create Solver =
+    # =================
+    if kargs.get('solver_type','classic') == 'classic':
+        solver = pyclaw.ClawSolver1D()
+    else:
+        raise NotImplementedError('Classic is currently the only supported solver.')
+        
+    # Solver method parameters
+    solver.cfl_desired = 0.9
+    solver.cfl_max = 1.0
+    solver.max_steps = 5000
+    solver.fwave = True
+    solver.kernel_language = 'Fortran'
+    solver.num_waves = 4
+    solver.limiters = 3
+    solver.source_split = 1
+        
+    # Boundary conditions
+    # Use wall boundary condition at beach
+    solver.bc_lower[0] = 1
+    solver.bc_upper[0] = 0
+    solver.user_bc_upper = ml.bc.wall_qbc_upper
+    solver.aux_bc_lower[0] = 1
+    solver.aux_bc_upper[0] = 1
+    
+    # Set the Riemann solver
+    solver.rp = riemann.rp1_layered_shallow_water
+
+    # Set the before step function
+    solver.before_step = lambda solver,solution:ml.solver.before_step(solver,
+                                                                      solution,
+                                                                      raise_on_fail=False)
+                                            
+    # Use simple friction source term
+    solver.step_source = ml.source.friction_source
+
+    
+    # ============================
+    # = Create Initial Condition =
+    # ============================
+    num_layers = 2
+    
+    x = pyclaw.Dimension('x',-400e3,0.0,num_cells)
+    domain = pyclaw.Domain([x])
+    state = pyclaw.State(domain,2*num_layers,3+num_layers)
+    state.aux[ml.aux.kappa_index,:] = 0.0
+
+    # Set physics data
+    state.problem_data['g'] = 9.8
+    state.problem_data['manning'] = 0.0
+    state.problem_data['rho_air'] = 1.15
+    state.problem_data['rho'] = [1025.0,1045.0]
+    state.problem_data['r'] = state.problem_data['rho'][0] / state.problem_data['rho'][1]
+    state.problem_data['one_minus_r'] = 1.0 - state.problem_data['r']
+    state.problem_data['num_layers'] = num_layers
+    
+    # Set method parameters, this ensures it gets to the Fortran routines
+    state.problem_data['eigen_method'] = eigen_method
+    state.problem_data['dry_tolerance'] = 1e-3
+    state.problem_data['inundation_method'] = 2
+    state.problem_data['entropy_fix'] = False
+    
+    solution = pyclaw.Solution(state,domain)
+    solution.t = 0.0
     
     # Set aux arrays including bathymetry, wind field and linearized depths
     x0 = -130e3
     x1 = -30e3
-    ml.set_sloped_shelf_bathymetry(solution.state,x0,x1,-4000.0,-100.0)
-    wind.set_no_wind(solution.state)
-    ml.set_h_hat(solution.state,0.5,[0.0,-300.0],[0.0,-300.0])
-    ml.set_acta_numerica_init_condition(solution.state,0.4)
+    ml.aux.set_sloped_shelf_bathymetry(solution.state,x0,x1,-4000.0,-100.0)
+    ml.aux.set_no_wind(solution.state)
+    ml.aux.set_h_hat(solution.state,0.5,[0.0,-300.0],[0.0,-300.0])
     
-    # Set output
-    controller.outdir = outdir
-    controller.num_output_times = 300
+    # Set perturbation to sea at rest
+    ml.qinit.set_acta_numerica_init_condition(solution.state,0.4)
+    
+    
+    # ================================
+    # = Create simulation controller =
+    # ================================
+    controller = pyclaw.Controller()
+    controller.solution = solution
+    controller.solver = solver
+    
+    # Output parameters
+    controller.output_style = 1
     controller.tfinal = 7200.0
+    controller.num_output_times = 300
+    controller.write_aux_init = True
+    controller.outdir = outdir
     controller.write_aux = True
     
-    # Run simulation
+    # ==================
+    # = Run Simulation =
+    # ==================
     state = controller.run()
     
-    # Plotting
+    
+    # ============
+    # = Plotting =
+    # ============
     plot_kargs = {"eta":[0.0,-300.0],
                   "rho":solution.state.problem_data['rho'],
                   "g":solution.state.problem_data['g'],
@@ -201,7 +278,7 @@ if __name__ == "__main__":
     else:
         eig_methods = [1,2,3,4]
         
-    for method in eig_methods:
-        jump_shelf(2000,method,iplot=False,htmlplot=True)
     # for method in eig_methods:
-    #     sloped_shelf(2000,method,iplot=False,htmlplot=True)
+    #     jump_shelf(2000,method,iplot=False,htmlplot=True)
+    for method in eig_methods:
+        sloped_shelf(2000,method,iplot=False,htmlplot=True)
