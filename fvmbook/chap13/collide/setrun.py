4f98bd5e937a5cc0c3c7a86e6df5e0e9f9412c50
Randy LeVeque <rjl@ned>
2013-10-28 23:06:58 -0700

    fix == errors in setrun

--- fvmbook/chap13/collide/setrun.py
+++ fvmbook/chap13/collide/setrun.py
@@ -121,7 +121,7 @@ def setrun(claw_pkg='classic'):
         clawdata.output_t0 = True  # output at initial (or restart) time?
         
 
-    clawdata.output_format == 'ascii'      # 'ascii', 'binary', 'netcdf'
+    clawdata.output_format = 'ascii'      # 'ascii', 'binary', 'netcdf'
 
     clawdata.output_q_components = 'all'   # could be list such as [True,True]
     clawdata.output_aux_components = 'none'  # could be list
