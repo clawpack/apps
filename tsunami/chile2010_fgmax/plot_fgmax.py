1b3f6362ebfdf0bafe92ffc5b17f43c5987cb560
Randy LeVeque <rjl@uw.edu>
2014-09-09 15:57:09 -0400

    added new plot_fgmax.py and removed old versions, -- not doing transect plot now

---
+++ tsunami/chile2010_fgmax/plot_fgmax.py
@@ -0,0 +1,42 @@
+"""
+Plot fgmax output from GeoClaw run.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy
+from clawpack.geoclaw import fgmax_tools, geoplot
+
+def plot_fgmax_grid():
+
+    fg = fgmax_tools.FGmaxGrid()
+    fg.read_input_data('fgmax_grid.txt')
+    fg.read_output()
+
+    clines_zeta = [0.01] + list(numpy.linspace(0.05,0.3,6)) + [0.5,1.0,10.0]
+    colors = geoplot.discrete_cmap_1(clines_zeta)
+    plt.figure(1)
+    plt.clf()
+    plt.contourf(fg.x,fg.y,fg.zeta,clines_zeta,colors=colors)
+    plt.contour(fg.x,fg.y,fg.B,[0.],colors='k')  # coastline
+
+    # plot arrival time contours and label:
+    arrival_t = fg.arrival_time/3600.  # arrival time in hours
+    clines_t = numpy.linspace(0,8,17)  # hours
+    clines_t_label = clines_t[::2]  # which ones to label 
+    clines_t_colors = [.5,.5,.5]
+    con_t = plt.contour(fg.x,fg.y,arrival_t, clines_t,colors=clines_t_colors) 
+    plt.clabel(con_t, clines_t_label)
+
+    # fix axes:
+    plt.ticklabel_format(format='plain',useOffset=False)
+    plt.xticks(rotation=20)
+    plt.gca().set_aspect(1./numpy.cos(fg.y.mean()*numpy.pi/180.))
+    plt.title("Maximum amplitude / arrival times")
+
+
+if __name__=="__main__":
+    plot_fgmax_grid()
+    fname = "amplitude_times.png"
+    plt.savefig(fname)
+    print "Created ",fname
