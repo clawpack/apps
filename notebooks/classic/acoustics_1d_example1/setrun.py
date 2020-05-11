87b65bad03d79178eac9248a7945f95c084ef137
Randy LeVeque <rjl@uw.edu>
2020-05-11 10:41:16 -0700

    added notebooks/Makefile and fixed classic notebooks

--- notebooks/classic/acoustics_1d_example1/setrun.py
+++ notebooks/classic/acoustics_1d_example1/setrun.py
@@ -60,7 +60,7 @@ def setrun(claw_pkg='classic'):
     clawdata.upper[0] = 1.000000e+00          # xupper
     
     # Number of grid cells:
-    clawdata.num_cells[0] = 400      # mx
+    clawdata.num_cells[0] = 100      # mx
     
 
     # ---------------
