12321e4ff1d68c0875ef6e018cbea241c949f5fc
Randy LeVeque <rjl@uw.edu>
2023-01-26 13:18:03 -0800

    fix some things in fvmbook/chap7/standing, switch to 50 cells

--- fvmbook/chap7/standing/qinit.f
+++ fvmbook/chap7/standing/qinit.f
@@ -11,15 +11,14 @@ c
       implicit double precision (a-h,o-z)
       dimension q(meqn,1-mbc:mx+mbc)
       dimension aux(maux,1-mbc:mx+mbc)
-      common /cqinit/ beta
 c
 c
       pi = 4.d0*datan(1.d0)
-      do 150 i=1,mx
-	 xcell = xlower + (i-0.5d0)*dx
-	 q(1,i) = dcos(2.d0*pi*xcell)
-	 q(2,i) = 0.d0
-  150    continue
+      do i=1,mx
+         xcell = xlower + (i-0.5d0)*dx
+         q(1,i) = dcos(2.d0*pi*xcell)
+         q(2,i) = 0.d0
+      enddo
 c
       return
       end
